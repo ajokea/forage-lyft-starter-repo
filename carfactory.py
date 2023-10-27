360a1eca437889e2feeff6694490c962b140943a
ajokea <ajokenakinseye@gmail.com>
2023-10-27 17:55:54 -0400

    refactoring codebase

---
+++ carfactory.py
@@ -0,0 +1,22 @@
+from car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+from battery.spindler_battery import SpindlerBattery
+from battery.nubbin_battery import NubbinBattery
+
+class CarFactory:
+    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        return Car(CapuletEngine(last_service_mileage,current_mileage), SpindlerBattery(last_service_date, current_date))
+
+    def create_thorex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        return Car(CapuletEngine(last_service_mileage,current_mileage), NubbinBattery(last_service_date, current_date))
+
+    def create_palindrome(self, current_date, last_service_date, warning_light_on):
+        return Car(SternmanEngine(last_service_date, warning_light_on), SpindlerBattery(last_service_date, current_date))
+
+    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        return Car(WilloughbyEngine(last_service_mileage,current_mileage), SpindlerBattery(last_service_date, current_date))
+
+    def create_rorsacch(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        return Car(WilloughbyEngine(last_service_mileage,current_mileage), NubbinBattery(last_service_date, current_date))
---
+++ serviceable.py
@@ -0,0 +1,5 @@
+from abc import abstractmethod
+
+@abstractmethod
+def needs_service():
+    pass
