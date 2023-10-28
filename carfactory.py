c353628c1ccf58a43b8ad7388e343a457f6de8c2
ajokea <ajokenakinseye@gmail.com>
2023-10-27 21:15:07 -0400

    refactoring

--- carfactory.py
+++ carfactory.py
@@ -6,17 +6,17 @@ from battery.spindler_battery import SpindlerBattery
 from battery.nubbin_battery import NubbinBattery
 
 class CarFactory:
-    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        return Car(CapuletEngine(last_service_mileage,current_mileage), SpindlerBattery(last_service_date, current_date))
+    def create_calliope(last_service_date, current_mileage, last_service_mileage):
+        return Car(CapuletEngine(last_service_mileage,current_mileage), SpindlerBattery(last_service_date))
 
-    def create_thorex(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        return Car(CapuletEngine(last_service_mileage,current_mileage), NubbinBattery(last_service_date, current_date))
+    def create_thorex(last_service_date, current_mileage, last_service_mileage):
+        return Car(CapuletEngine(last_service_mileage,current_mileage), NubbinBattery(last_service_date))
 
-    def create_palindrome(self, current_date, last_service_date, warning_light_on):
-        return Car(SternmanEngine(warning_light_on), SpindlerBattery(last_service_date, current_date))
+    def create_palindrome(last_service_date, warning_light_on):
+        return Car(SternmanEngine(warning_light_on), SpindlerBattery(last_service_date))
 
-    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        return Car(WilloughbyEngine(last_service_mileage,current_mileage), SpindlerBattery(last_service_date, current_date))
+    def create_glissade(last_service_date, current_mileage, last_service_mileage):
+        return Car(WilloughbyEngine(last_service_mileage,current_mileage), SpindlerBattery(last_service_date))
 
-    def create_rorsacch(self, current_date, last_service_date, current_mileage, last_service_mileage):
-        return Car(WilloughbyEngine(last_service_mileage,current_mileage), NubbinBattery(last_service_date, current_date))
+    def create_rorsacch(last_service_date, current_mileage, last_service_mileage):
+        return Car(WilloughbyEngine(last_service_mileage,current_mileage), NubbinBattery(last_service_date))
